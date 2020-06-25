a229478bdee95f89a647e665d2830751080ff276
Edoardo Morandi <morandidodo@gmail.com>
2020-06-25 23:43:43 +0200

    Add case-sensitive test for exercise 2

--- ownership/exercise_02.rs
+++ ownership/exercise_02.rs
@@ -2,9 +2,12 @@
 mod tests {
     use super::*;
 
+    const DATA: [&str; 3] = ["aIebaeabbioubbAiebaiE", "baiebbiouioub", "ioubioub"];
+
+    // Remove one of the test if you want to test only one version of the exercise
+
     #[test]
     fn count_vowel_slices_occurrences() {
-        const DATA: [&str; 3] = ["aIebaeabbioubbAiebaiE", "baiebbiouioub", "ioubioub"];
         let mut result: Vec<_> = vowels_slices_occurrences(&DATA)
             .into_iter()
             .map(|(slice, occurrences)| (slice.to_lowercase(), occurrences))
@@ -21,5 +24,26 @@ mod tests {
             ]
         );
     }
-}
 
+    #[test]
+    fn count_vowel_slices_occurrences_case_sensitive() {
+        let mut result: Vec<_> = vowels_slices_occurrences_case_sensitive(&DATA)
+            .into_iter()
+            .map(|(slice, occurrences)| (slice, occurrences))
+            .collect();
+        result.sort_unstable_by(|(a, _), (b, _)| a.cmp(b));
+
+        assert_eq!(
+            result,
+            vec![
+                ("Aie", 1),
+                ("aIe", 1),
+                ("aea", 1),
+                ("aiE", 1),
+                ("aie", 1),
+                ("iou", 3),
+                ("iouiou", 1),
+            ]
+        );
+    }
+}
